75a19b6fb9fd1791d8befc16199ba8e29003152f
KareemAbbas1 <82244782+KareemAbbas1@users.noreply.github.com>
2022-03-29 21:19:51 +0200

    API problem solved

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import Icon from '../assets/Current Icon.png'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const MainView = () => {
+
+    const [data, setData] = useState();
 
     //API Key
     const API_KEY = "a177f8481c31fa96c3f95ad4f4f84610";
@@ -11,7 +13,8 @@ const MainView = () => {
     // Create a new Date instance
     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let d = new Date();
-    let newDate = weekday[d.getDay()] + '.' + d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
+    let m = d.getMonth() + 1;
+    let newDate = `${weekday[d.getDay()]}, ${m}.${d.getDate()}.${d.getFullYear()}`;
 
     const x = document.getElementById("location")
     const getLocation = () => {
@@ -20,35 +23,32 @@ const MainView = () => {
         } else {
             x.innerHTML = "Geolocation is not supported on this browser"
         };
+        // return getLocation();
     };
-    
-    useEffect()
-    const getWeatherData = async (position) => {
+
+    useEffect(() => {
+        getLocation();
+    }, [])
+
+    const getWeatherData = (position) => {
         let latitude = position.coords.latitude;
         let longitude = position.coords.longitude;
 
-        const URL = `https://api.darksky.net/forecast/${API_KEY}/${latitude}, ${longitude}`
-
-        const request = await fetch(URL, {
-            mode: 'no-cors',
-            method: 'GET',
-            credentials: 'same-origin',
-        })
-        try {
-            return await request.json()
-        }
-
-        catch (error) {
-            console.log('error', error);
-        }
-        console.log('temp', request)
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${API_KEY}/${latitude},${longitude}`)
+            .then(res => res.json())
+            .then(data => setData(data))
+            .catch(error => console.log("Error", error))
     };
 
-
+    let fTemp = data ? data.currently.temperature : null;
+    let cTemp = Math.round((fTemp - 32) / 1.8);
+    let summary = data ? data.currently.summary : null;
+    // let icon = data ? data.currently.icon : null;
+    // console.log(temp)
+    console.log(data)
     return (
         <Container>
-            <button onClick={getLocation()}>get location</button>
-            <div className='pt-5 d-flex justify-content-between align-items-center text-white'>
+            <div className='pt-5 d-flex justify-content-between align-items-center '>
                 <div className='brand'><h2>INSTAWEATER</h2></div>
                 <span className='d-flex justify-content-between'>
                     <div className='measurement'><h2 className='text-center'>C</h2></div>
@@ -59,15 +59,19 @@ const MainView = () => {
 
             <div className='pt-5 mt-5 mb-5 d-flex justify-content-between'>
                 <div>
-                    <h1 id='location' className='text-white'>City Name</h1>
-                    <p className='text-white'>{newDate}</p>
+                    <h1 id='location' className=''>City Name</h1>
+                    <p className=''><strong>{newDate}</strong></p>
                     <div>
                         <img src={Icon} alt='weather icon' className='mt-4'></img>
+                    </div>
+                    <div className='mt-3'>
+                        <h2>{summary}</h2>
                     </div>
                 </div>
 
                 <div>
-                    <h2 className='text-white me-4'>Temp</h2>
+                    <h2 className=' me-4'>{fTemp}</h2>
+                    <h2 className=' me-4'>{cTemp}</h2>
                 </div>
             </div>
 
@@ -80,3 +84,13 @@ const MainView = () => {
 }
 
 export default MainView
+
+
+
+// if (res.ok) {
+//     console.log('Success')
+//         .then(res => res.json())
+// }else {
+//     console.log("Failed to fetch data")
+// }
+// }
